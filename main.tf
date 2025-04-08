a97c0adde37e5b651584e8712f7d4cea35a3f934
Abdul Faiyaz <abdulfaiyaz.shukor@gmail.com>
2025-04-08 23:01:53 +0530

    fix: aws iam policy

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# --- VPC and Subnet Setup ---
 data "aws_vpc" "existing_vpc" {
   default = true
 }
-
 
 resource "aws_vpc" "k8s_vpc" {
   count      = length(data.aws_vpc.existing_vpc.id) > 0 ? 0 : 1
@@ -16,7 +16,6 @@ locals {
   vpc_id = try(data.aws_vpc.existing_vpc.id, aws_vpc.k8s_vpc[0].id)
 }
 
-# --- Check for existing subnets ---
 data "aws_subnets" "existing_subnets" {
   filter {
     name   = "vpc-id"
@@ -24,18 +23,17 @@ data "aws_subnets" "existing_subnets" {
   }
 }
 
-# --- Create subnets only if they don't exist ---
 resource "aws_subnet" "subnet_1" {
   count             = length(data.aws_subnets.existing_subnets.ids) > 0 ? 0 : 1
-  vpc_id           = local.vpc_id
-  cidr_block       = "10.0.1.0/24"
+  vpc_id            = local.vpc_id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
 }
 
 resource "aws_subnet" "subnet_2" {
   count             = length(data.aws_subnets.existing_subnets.ids) > 1 ? 0 : 1
-  vpc_id           = local.vpc_id
-  cidr_block       = "10.0.2.0/24"
+  vpc_id            = local.vpc_id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-south-1b"
 }
 
@@ -43,7 +41,7 @@ locals {
   subnet_ids = length(data.aws_subnets.existing_subnets.ids) > 0 ? data.aws_subnets.existing_subnets.ids : [aws_subnet.subnet_1[0].id, aws_subnet.subnet_2[0].id]
 }
 
-# --- Security Group for EKS ---
+# --- Security Group ---
 resource "aws_security_group" "eks_sg" {
   vpc_id = local.vpc_id
 
@@ -73,7 +71,7 @@ locals {
   security_group_id = aws_security_group.eks_sg.id
 }
 
-# --- IAM Role for EKS ---
+# --- IAM Role for EKS Cluster ---
 data "aws_iam_role" "existing_eks_role" {
   name = "eks-cluster-role"
 }
@@ -96,7 +94,38 @@ locals {
   eks_role_arn = try(data.aws_iam_role.existing_eks_role.arn, aws_iam_role.eks_role[0].arn)
 }
 
-# --- AWS ECR Repository ---
+# --- IAM Role for EKS Node Group ---
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_worker_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_ecr_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+# --- ECR Repository ---
 resource "aws_ecr_repository" "medicure_repo" {
   name                 = "medicure-repo"
   image_tag_mutability = "MUTABLE"
@@ -110,12 +139,40 @@ resource "aws_ecr_repository" "medicure_repo" {
 resource "aws_eks_cluster" "k8s_cluster" {
   name     = "health-cluster"
   role_arn = local.eks_role_arn
-  version  = "1.29" # Update based on latest stable version
+  version  = "1.29"
 
   vpc_config {
     subnet_ids         = local.subnet_ids
     security_group_ids = [local.security_group_id]
   }
+}
+
+# --- EKS Node Group ---
+resource "aws_eks_node_group" "default" {
+  cluster_name    = aws_eks_cluster.k8s_cluster.name
+  node_group_name = "healthcare-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = local.subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+  ami_type       = "AL2_x86_64"
+
+  tags = {
+    Name = "healthcare-node-group"
+  }
+
+  depends_on = [
+    aws_eks_cluster.k8s_cluster,
+    aws_iam_role_policy_attachment.eks_node_worker_policy,
+    aws_iam_role_policy_attachment.eks_node_ecr_policy,
+    aws_iam_role_policy_attachment.eks_node_cni_policy
+  ]
 }
 
 # --- Outputs ---
